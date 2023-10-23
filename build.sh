cd8019729ebe666c54ce30f5230da1c4e811a128
nater1983 <naterussell83@gmail.com>
2023-10-22 19:57:47 -0500

    Update

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+PSB=/var/cache/psb
+
+if [ ! -d ${PSB} ]; then
+	mkdir -p $PSB
+fi
+
+src=(
+	elementary-tasks 
+	switchboard-plug-wacom 
+	switchboard-plug-sharing 
+	switchboard-plug-bluetooth 
+	elementary-camera 
+	elementary-calendar 
+	wingpanel-indicator-nightlight 
+	elementary-contractor 
+	elementary-print 
+	elementary-fonts 
+	wingpanel-indicator-network 
+	indicator-application 
+	lightdm 
+	lightdm-gtk-greeter 
+	lightdm-pantheon-greeter
+)
+
+for i in ${src[@]}; do
+	cd ${i} || exit 1
+	sh ${i}.SlackBuild || exit 1
+	cd ..
+done
+
+exit 0
+
+
---
+++ elementary-fonts/doinst.sh
@@ -0,0 +1,6 @@
+
+if [ -x /usr/bin/fc-cache ]; then
+/usr/bin/fc-cache -f >/dev/null 2>&1
+fi
+
+
