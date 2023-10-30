4d627bb843fabcf36eb8989ab00bf0555267ce53
nater1983 <naterussell83@gmail.com>
2023-10-29 19:52:41 -0500

    Update

---
+++ build.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+PSB=/var/cache/psb
+
+if [ ! -d ${PSB} ]; then
+	mkdir -p $PSB
+fi
+
+src=(
+        telepathy-glib
+	wingpanel 
+	switchboard 
+	python3-rdflib 
+	zeitgeist 
+	pantheon-applications-menu
+        cerbere
+        python-onboard
+        liblouis
+        orca
+	pantheon-agent-polkit 
+	wingpanel-indicator-session 
+	pantheon-session 
+        tumbler	
+	elementary-wallpapers 
+	elementary-dock 
+	switchboard-plug-pantheon-shell
+	libstemmer 
+	AppStream 
+	wingpanel-indicator-keyboard
+	libgnomekbd 
+	switchboard-plug-keyboard 
+	wingpanel-indicator-sound 
+	switchboard-plug-sound
+	webkit2gtk4.1
+	evolution-data-server 
+	wingpanel-indicator-datetime 
+	switchboard-plug-datetime 
+	libcloudproviders
+        http-parser
+        libgit2
+        libgit2-glib
+	pantheon-filemanager 
+	gtk-theme-elementary 
+	pantheon-icons 
+	pantheon-terminal 
+	pantheon-tweaks 
+	switchboard-plug-network
+        librest
+        webkit2gtk4.1
+	elementary-photos 
+	bash-completion 
+	wingpanel-indicator-power 
+	switchboard-plug-power 
+	switchboard-plug-notifications 
+	wingpanel-indicator-notifications 
+	switchboard-plug-display 
+	gtkspell3
+	gtksourceview4
+	libpeas 
+	elementary-code
+	folks 
+	elementary-mail 
+	switchboard-plug-onlineaccounts 
+	pantheon-default-settings 
+	pantheon-screenshot
+	bubblewrap
+	libostree
+	flatpak
+	pantheon-sideload
+	cogl
+	clutter
+	clutter-gst 
+	pantheon-videos 
+	switchboard-plug-applications 
+	ayatana-ido 
+	libayatana-indicator 
+	libgdiplus 
+	mono 
+	gtk-sharp 
+	perl-xml-libxml 
+	libayatana-appindicator 
+	ayatana-indicator-application 
+	wingpanel-indicator-ayatana 
+	switchboard-plug-about 
+	switchboard-plug-locale 
+	switchboard-plug-useraccounts 
+	switchboard-plug-security-privacy 
+	switchboard-plug-a11y 
+	switchboard-plug-mouse-touchpad 
+	pantheon-settings-daemon 
+	libgda 
+	elementary-music 
+	switchboard-plug-printers
+	libadwaita 
+	pantheon-onboarding
+	libchamplain
+	gnome-online-accounts
+	libgdata 
+	elementary-tasks 
+	switchboard-plug-wacom 
+	switchboard-plug-sharing 
+	switchboard-plug-bluetooth 
+	elementary-camera 
+	elementary-calendar 
+	wingpanel-indicator-nightlight 
+	elementary-contractor 
+	elementary-print 
+	elementary-fonts 
+	wingpanel-indicator-network 
+	indicator-application 
+	lightdm 
+	lightdm-pantheon-greeter
+	xdg-desktop-portal-pantheon
+)
+
+for i in ${src[@]}; do
+	cd ${i} || exit 1
+	sh ${i}.SlackBuild || exit 1
+	cd ..
+done
+
+exit 0
+
+
