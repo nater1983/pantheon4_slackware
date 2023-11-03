f5f594769738e0aa501e6c3c40655d3721e2e32d
nater1983 <naterussell83@gmail.com>
2023-11-02 21:33:25 -0500

    Update

--- build.sh
+++ build.sh
@@ -6,68 +6,6 @@ if [ ! -d ${PSB} ]; then
 fi
 
 src=(
-	pantheon-tweaks 
-	switchboard-plug-network
-        librest
-	elementary-photos 
-	bash-completion 
-	wingpanel-indicator-power 
-	switchboard-plug-power 
-	switchboard-plug-notifications 
-	wingpanel-indicator-notifications 
-	switchboard-plug-display 
-	gtkspell3
-	gtksourceview4
-	libpeas 
-	elementary-code
-	folks 
-	elementary-mail 
-	switchboard-plug-onlineaccounts 
-	pantheon-default-settings 
-	pantheon-screenshot
-	bubblewrap
-	libostree
-	flatpak
-	pantheon-sideload
-	cogl
-	clutter
-	clutter-gst 
-	pantheon-videos 
-	switchboard-plug-applications 
-	ayatana-ido 
-	libayatana-indicator 
-	libgdiplus 
-	mono 
-	gtk-sharp 
-	perl-xml-libxml 
-	libayatana-appindicator 
-	ayatana-indicator-application 
-	wingpanel-indicator-ayatana 
-	switchboard-plug-about 
-	switchboard-plug-locale 
-	switchboard-plug-useraccounts 
-	switchboard-plug-security-privacy 
-	switchboard-plug-a11y 
-	switchboard-plug-mouse-touchpad 
-	pantheon-settings-daemon 
-	libgda 
-	elementary-music 
-	switchboard-plug-printers
-	libadwaita 
-	pantheon-onboarding
-	libchamplain
-	gnome-online-accounts
-	libgdata 
-	elementary-tasks 
-	switchboard-plug-wacom 
-	switchboard-plug-sharing 
-	switchboard-plug-bluetooth 
-	elementary-camera 
-	elementary-calendar 
-	wingpanel-indicator-nightlight 
-	elementary-contractor 
-	elementary-print 
-	elementary-fonts 
 	wingpanel-indicator-network 
 	indicator-application 
 	lightdm 
